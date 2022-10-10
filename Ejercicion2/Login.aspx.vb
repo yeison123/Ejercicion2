46780a956d864ed8c3f55e6b12d199f926cbae7b
yeison123 <yeison_123.2@hotmail.com>
2022-10-09 19:36:54 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicion2/Login.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub SqlDataSource1_Selecting(sender As Object, e As SqlDataSourceSelectingEventArgs) Handles SqlDataSource1.Selecting
+
+    End Sub
+End Class
